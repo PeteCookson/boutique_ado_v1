3f77dd01ce48ed52accc0e28f2a64334893af76a
PeteCookson <pgcookson@gmail.com>
2022-08-09 20:47:26 +0000

    Added STATIC_ROOT to settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -183,6 +183,7 @@ if 'USE_AWS' in os.environ:
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
